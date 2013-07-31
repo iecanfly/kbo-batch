c944cbf4682cf675bdd6f99e61c9bf151da49875
iecanfly <iecanfly@gmail.com>
2013-07-31 16:23:07 +0800

    debugging

--- src/main/java/com/iecanfly/kbo/ExampleItemWriter.java
+++ src/main/java/com/iecanfly/kbo/ExampleItemWriter.java
@@ -20,7 +20,9 @@ public class ExampleItemWriter implements ItemWriter<Object> {
 	 * @see ItemWriter#write(java.util.List)
 	 */
 	public void write(List<? extends Object> data) throws Exception {
+
 		log.info(data);
+        throw new Exception();
 	}
 
 }
