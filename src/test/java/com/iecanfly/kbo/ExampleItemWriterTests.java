e8296cbd9f9fe757e860fcb5e8fe30c73296dfd1
iecanfly <iecanfly@gmail.com>
2013-07-31 15:24:18 +0800

    init commit

---
+++ src/test/java/com/iecanfly/kbo/ExampleItemWriterTests.java
@@ -0,0 +1,15 @@
+package com.iecanfly.kbo;
+
+import com.iecanfly.kbo.ExampleItemWriter;
+
+import junit.framework.TestCase;
+
+public class ExampleItemWriterTests extends TestCase {
+
+	private ExampleItemWriter writer = new ExampleItemWriter();
+	
+	public void testWrite() throws Exception {
+		writer.write(null); // nothing bad happens
+	}
+
+}
